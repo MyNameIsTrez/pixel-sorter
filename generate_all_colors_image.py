22326b1b3c7121c7ec1d99096ecb0d2207254c6e
MyNameIsTrez <welfje@gmail.com>
2023-05-21 12:59:38 +0200

    Add generate_all_colors_image.py

---
+++ generate_all_colors_image.py
@@ -0,0 +1,26 @@
+import math
+from pathlib import Path
+
+from PIL import Image
+
+
+def main():
+    pixel_count = 256**3
+    side_length = math.isqrt(pixel_count)
+
+    img = Image.new("RGB", (side_length, side_length), "black")
+    pixels = img.load()
+
+    for b in range(256):
+        for g in range(256):
+            for r in range(256):
+                col = r + ((b % 16) * 256)
+                row = g + ((b // 16) * 256)
+
+                pixels[col, row] = (r, g, b)
+
+    img.save("input/all_colors.png")
+
+
+if __name__ == "__main__":
+    main()
