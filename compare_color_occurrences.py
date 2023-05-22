bb92f72c720ce6de7cb221817eb90168aaaabc41
MyNameIsTrez <welfje@gmail.com>
2023-05-22 16:31:44 +0200

    Add big_palette.png

--- compare_color_occurrences.py
+++ compare_color_occurrences.py
@@ -2,6 +2,7 @@ import numpy as np
 from PIL import Image
 
 # filename = "all_colors.png"
+# filename = "big_palette.png"
 # filename = "elephant.png"
 # filename = "grid.png"
 filename = "palette.png"
--- shuffle.py
+++ shuffle.py
@@ -7,6 +7,7 @@ import pyopencl as cl
 from PIL import Image
 
 # filename = "all_colors.png"
+# filename = "big_palette.png"
 # filename = "elephant.png"
 # filename = "grid.png"
 filename = "palette.png"
