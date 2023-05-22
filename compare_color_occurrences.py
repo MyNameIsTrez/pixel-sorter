cbc0faa7ab35ac7062e88a54f20fedb0c793e10f
MyNameIsTrez <welfje@gmail.com>
2023-05-23 00:54:37 +0200

    Let compare_color_occurrences.py assert 2x again

--- compare_color_occurrences.py
+++ compare_color_occurrences.py
@@ -2,33 +2,34 @@ import numpy as np
 from PIL import Image
 
 # filename = "all_colors.png"
-filename = "big_palette.png"
+# filename = "big_palette.png"
 # filename = "elephant.png"
 # filename = "grid.png"
 # filename = "palette.png"
 # filename = "small.png"
-# filename = "tiny.png"
+filename = "tiny.png"
 
 
-def get_color_counts(filepath):
+def get_colors_and_counts(filepath):
     img = Image.open(filepath).convert("RGB")
 
     arr = np.array(img)
 
     # Arrange all pixels into a tall column of 3 RGB values and find unique rows (colors)
-    _, counts = np.unique(arr.reshape(-1, 3), axis=0, return_counts=1)
-    return counts
+    colors, counts = np.unique(arr.reshape(-1, 3), axis=0, return_counts=1)
+    return colors, counts
 
 
 def main():
-    input_counts = get_color_counts(f"input/{filename}")
-    output_counts = get_color_counts(f"output/{filename}")
+    input_colors, input_counts = get_colors_and_counts(f"input/{filename}")
+    output_colors, output_counts = get_colors_and_counts(f"output/{filename}")
 
+    colors_equal = np.array_equal(input_colors, output_colors)
     counts_equal = np.array_equal(input_counts, output_counts)
 
-    assert (
-        counts_equal
-    ), "❌ The color counts of the input and the output weren't identical!"
+    assert colors_equal, "❌ The set of colors of the input and output aren't identical!"
+
+    assert counts_equal, "❌ The color counts of the input and output aren't identical!"
 
     print("🎉 Images have identical color occurrences!")
 
