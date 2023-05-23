3940025242d21f0f2ab05bfec15ef7ad10f20eed
MyNameIsTrez <welfje@gmail.com>
2023-05-23 15:01:28 +0200

    Rename file to count_colors.py

--- count_colors.py
+++ count_colors.py
@@ -3,11 +3,11 @@ from PIL import Image
 
 # filename = "all_colors.png"
 # filename = "big_palette.png"
-# filename = "elephant.png"
+filename = "elephant.png"
 # filename = "grid.png"
 # filename = "palette.png"
 # filename = "small.png"
-filename = "tiny.png"
+# filename = "tiny.png"
 
 
 def get_colors_and_counts(filepath):
